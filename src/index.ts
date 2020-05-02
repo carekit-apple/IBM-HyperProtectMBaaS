82af0bdd0a5cc2dec70485c7a60dceac8ed1b969
Ry-is-me <43936556+Ry-is-me@users.noreply.github.com>
2020-05-02 16:37:45 -0400

    adding https code
    adding changes to index.ts to accommodate for https

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 /*
  Copyright (c) 2020, International Business Machines All rights reserved.
- 
+
  Redistribution and use in source and binary forms, with or without modification,
  are permitted provided that the following conditions are met:
- 
+
  1.  Redistributions of source code must retain the above copyright notice, this
  list of conditions and the following disclaimer.
- 
+
  2.  Redistributions in binary form must reproduce the above copyright notice,
  this list of conditions and the following disclaimer in the documentation and/or
  other materials provided with the distribution.
- 
+
  3. Neither the name of the copyright holder(s) nor the names of any contributors
  may be used to endorse or promote products derived from this software without
  specific prior written permission. No license is granted to the trademarks of
  the copyright holders even if such marks are included in this software.
- 
+
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,44 +28,70 @@
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import express from "express";
-import helmet from "helmet";
-import morgan from "morgan";
-import cors from "cors";
-import routes from "./routes/routes";
-import config = require("config");
+ import "reflect-metadata";
+ import { createConnection } from "typeorm";
+ import * as express from "express";
+ import * as helmet from "helmet";
+ import * as morgan from "morgan";
+ import * as cors from "cors";
+ import routes from "./routes/routes";
+ import * as config from "config";
+ import { createOrIncrementClock } from "./utils";
+ import * as dotenv from "dotenv";
+ import * as fs from "fs";
+ import * as https from "https";
 
-import { createOrIncrementClock } from "./utils";
+ dotenv.config();
+ const options = {
+   type: "mongodb",
+   database: "carekit",
+   entities: [
+     `${__dirname}/entity/**/*`,
+   ],
+   useNewUrlParser: true
+ };
 
-//import * as fs from "fs";
-//const ca = [fs.readFileSync(__dirname + "/cert.pem")];
+ // check env variable for MONGO_DB value - default is localhost (127.0.0.1)
+ if (process.env.MONGO_DB === "localhost") {
+   options.url = "mongodb://mongo:27017/mongo-test";
+ } else {
+   const ca = [fs.readFileSync(__dirname + "/cert.pem")];
+   options.url = process.env.MONGO_DB;
+   options.ssl = true;
+   options.sslCA = ca;
+ }
 
-createConnection()
-  .then(async (connection) => {
-    const app = express();
+ createConnection(options)
+   .then(async (connection) => {
+     const app = express();
+     app.use(cors());
+     app.use(helmet());
+     app.use(morgan("short"));
+     app.use(express.json());
 
-    app.use(cors());
-    app.use(helmet());
-    app.use(morgan("short"));
-    app.use(express.json());
+     app.use("/", routes);
 
-    app.use("/", routes);
+     if (app.get("env") === "production") {
+       app.use(function (req, res, next) {
+         const protocol = req.get("x-forwarded-proto");
+         protocol == "https" ? next() : res.redirect("https://" + req.hostname + req.url);
+       });
+     }
 
-    if (app.get("env") === "production") {
-      app.use(function (req, res, next) {
-        const protocol = req.get("x-forwarded-proto");
-        protocol == "https" ? next() : res.redirect("https://" + req.hostname + req.url);
-      });
-    }
+     await createOrIncrementClock(false);
 
-    await createOrIncrementClock(false);
+     const port = config.get("server.port");
 
-    const port = config.get("server.port");
+     app.get('/', function (req, res) {
+       res.send('Backend SDK is running')
+     })
 
-    app.listen(port, () => {
-      console.log("Server started on port " + port);
-    });
-  })
-  .catch((error) => console.log(error));
+     https.createServer({
+     key: fs.readFileSync('./certs/carekit-sdk.key'),
+     cert: fs.readFileSync('./certs/carekit-sdk.crt'),
+   }, app)
+     .listen(port, function () {
+       console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+     });
+   })
+     .catch((error) => console.log(error));
