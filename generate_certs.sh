ef07c069aac2e15b0409f11aa07860e3915ce59f
Ry-is-me <43936556+Ry-is-me@users.noreply.github.com>
2020-05-04 14:23:59 -0400

    Update generate_certs.sh

--- generate_certs.sh
+++ generate_certs.sh
@@ -4,15 +4,15 @@
 
 main() {
   # rootCA.key
-  openssl genrsa -out ~/HyperProtectBackendSDK-test/certs/rootCA.key 4096
+  openssl genrsa -out HyperProtectBackendSDK/certs/rootCA.key 4096
   # rootCA.crt
-  openssl req -x509 -new -nodes -key ~/HyperProtectBackendSDK-test/certs/rootCA.key -sha256 -days 1024 -out ~/HyperProtectBackendSDK-test/certs/rootCA.crt -subj "/C=US/ST=NC/O=IBM/OU=ZaaS/CN=$1"
+  openssl req -x509 -new -nodes -key HyperProtectBackendSDK/certs/rootCA.key -sha256 -days 1024 -out HyperProtectBackendSDK/certs/rootCA.crt -subj "/C=US/ST=NC/O=IBM/OU=ZaaS/CN=$1"
   # carekit-sdk.key
-  openssl genrsa -out ~/HyperProtectBackendSDK-test/certs/carekit-sdk.key 2048
+  openssl genrsa -out HyperProtectBackendSDK/certs/carekit-sdk.key 2048
   # carekit-sdk.csr
-  openssl req -new -sha256 -key ~/HyperProtectBackendSDK-test/certs/carekit-sdk.key -subj "/C=US/ST=NC/O=IBM_ZaaS/CN=$1" -out ~/HyperProtectBackendSDK-test/certs/carekit-sdk.csr
+  openssl req -new -sha256 -key HyperProtectBackendSDK/certs/carekit-sdk.key -subj "/C=US/ST=NC/O=IBM_ZaaS/CN=$1" -out HyperProtectBackendSDK/certs/carekit-sdk.csr
   # carekit-sdk.crt
-  openssl x509 -req -in ~/HyperProtectBackendSDK-test/certs/carekit-sdk.csr -CA ~/HyperProtectBackendSDK-test/certs/rootCA.crt -CAkey ~/HyperProtectBackendSDK-test/certs/rootCA.key -CAcreateserial -out ~/HyperProtectBackendSDK-test/certs/carekit-sdk.crt -days 500 -sha256
+  openssl x509 -req -in HyperProtectBackendSDK/certs/carekit-sdk.csr -CA HyperProtectBackendSDK/certs/rootCA.crt -CAkey HyperProtectBackendSDK/certs/rootCA.key -CAcreateserial -out HyperProtectBackendSDK/certs/carekit-sdk.crt -days 500 -sha256
 }
 
 main $1
