67e3a687fa747a0b3e10b897477942068ec6bd33
Elton-Desouza <elton.desouza@ca.ibm.com>
2020-05-06 14:15:56 -0400

    http vs https

--- src/index.ts
+++ src/index.ts
@@ -28,70 +28,65 @@
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
- import "reflect-metadata";
- import { createConnection } from "typeorm";
- import express = require("express");
- import helmet = require("helmet");
- import morgan = require("morgan");
- import cors = require("cors");
- import routes from "./routes/routes";
- import config = require("config");
- import { createOrIncrementClock } from "./utils";
- import dotenv = require("dotenv");
- import fs = require("fs");
- import https = require("https");
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import express = require("express");
+import helmet = require("helmet");
+import morgan = require("morgan");
+import cors = require("cors");
+import routes from "./routes/routes";
+import config = require("config");
+import { createOrIncrementClock } from "./utils";
+import dotenv = require("dotenv");
+import fs = require("fs");
+import https = require("https");
+import http = require("http");
 
- dotenv.config();
- const options = {
-   type: "mongodb",
-   database: "carekit",
-   entities: [
-     `${__dirname}/entity/**/*`,
-   ],
-   useNewUrlParser: true
- };
+dotenv.config();
+const options = {
+  type: "mongodb",
+  database: "carekit",
+  entities: [`${__dirname}/entity/**/*`],
+  useNewUrlParser: true,
+};
 
- // check env variable for MONGO_DB value - default is localhost (127.0.0.1)
- if (process.env.MONGO_DB === "localhost") {
-   options.url = "mongodb://mongo:27017/mongo-test";
- } else {
-   const ca = [fs.readFileSync(__dirname + "/cert.pem")];
-   options.url = process.env.MONGO_DB;
-   options.ssl = true;
-   options.sslCA = ca;
- }
+// check env variable for MONGO_DB value - default is localhost (127.0.0.1)
+if (process.env.MONGO_DB === "localhost") {
+  options.url = "mongodb://mongo:27017/mongo-test";
+} else {
+  const ca = [fs.readFileSync(__dirname + "/cert.pem")];
+  options.url = process.env.MONGO_DB;
+  options.ssl = true;
+  options.sslCA = ca;
+}
 
- createConnection(options)
-   .then(async (connection) => {
-     const app = express();
-     app.use(cors());
-     app.use(helmet());
-     app.use(morgan("short"));
-     app.use(express.json());
+createConnection(options)
+  .then(async (connection) => {
+    const app = express();
+    app.use(cors());
+    app.use(helmet());
+    app.use(morgan("short"));
+    app.use(express.json());
 
-     app.use("/", routes);
+    app.use("/", routes);
 
-     if (app.get("env") === "production") {
-       app.use(function (req, res, next) {
-         const protocol = req.get("x-forwarded-proto");
-         protocol == "https" ? next() : res.redirect("https://" + req.hostname + req.url);
-       });
-     }
+    if (app.get("env") === "production") {
+      app.use(function (req, res, next) {
+        const protocol = req.get("x-forwarded-proto");
+        protocol == "https" ? next() : res.redirect("https://" + req.hostname + req.url);
+      });
+    }
 
-     await createOrIncrementClock(false);
+    await createOrIncrementClock(false);
 
-     const port = config.get("server.port");
+    const port = config.get("server.port");
 
-     app.get('/', function (req, res) {
-       res.send('Backend SDK is running')
-     })
+    app.get("/", function (req, res) {
+      res.send("Backend SDK is running");
+    });
 
-     https.createServer({
-     key: fs.readFileSync('./certs/carekit-sdk.key'),
-     cert: fs.readFileSync('./certs/carekit-sdk.crt'),
-   }, app)
-     .listen(port, function () {
-       console.log('Example app listening on port 3000! Go to https://localhost:3000/')
-     });
-   })
-     .catch((error) => console.log(error));
+    http.createServer(app).listen(port, function () {
+      console.log("Example app listening on port 3000! Go to http://localhost:3000/");
+    });
+  })
+  .catch((error) => console.log(error));
