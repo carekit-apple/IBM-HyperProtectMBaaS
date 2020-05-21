073eae2569f51404725265902c5f86a1f7d1a623
Ry-is-me <43936556+Ry-is-me@users.noreply.github.com>
2020-05-21 16:06:50 -0400

    Create BackendSDKTest.spec.js
    Code to run `npm run test`. This code will run a validation test that outputs the results using the jest npm package. If the test fails, the output will state what caused the failure.

---
+++ __test__/BackendSDKTest.spec.js
@@ -0,0 +1,35 @@
+/* BackendSDK CareKit Test
+  - using 'jest' npm package
+  - POST is issued using 'verification.json' file as data
+  
+  Use command 'npm run test' to initiate validation test
+*/
+
+const fs = require('fs');
+const path = require('path');
+const request = require('request');
+
+describe("BackendSDK CareKit Function", () => {
+  test("Testing POST call to Backend SDK", () => {
+    // code to test goes here
+    const input = require(path.resolve(__dirname + '../../verification.json'));
+    const cacert = fs.readFileSync(path.resolve(__dirname + '../../src/carekit-root.crt'));
+    const options = {
+      url: 'https://localhost:3000/revisionRecord',
+      method: 'POST',
+      headers: 'Content-Type: application/json',
+      body: input,
+      json: true,
+      ca: cacert
+    };
+
+    const output = async (options) => {
+      const response = await request.post(options, function(err, res, body) {
+        if(err) {
+          console.log(`Error: ${err}`);
+        };
+      });
+      return response;
+    }
+    output(options);
+  });
