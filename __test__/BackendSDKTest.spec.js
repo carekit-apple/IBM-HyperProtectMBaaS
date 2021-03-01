9d9387469b503e0587a547b9dfee42eda079a35a
Elton de Souza <elton.desouza@ca.ibm.com>
2021-03-01 15:52:11 -0500

    Add GET tests along with POST

--- __test__/BackendSDKTest.spec.js
+++ __test__/BackendSDKTest.spec.js
@@ -8,14 +8,15 @@
 const fs = require('fs');
 const path = require('path');
 const request = require('request');
+const diff = require('json-diff');
 
-describe("BackendSDK CareKit Function", () => {
+describe("Testing MBaaS functionality", () => {
   test("Testing POST call to Backend SDK", () => {
     // code to test goes here
     const input = require(path.resolve(__dirname + '../../verification.json'));
     const cacert = fs.readFileSync(path.resolve(__dirname + '../../src/carekit-root.crt'));
     const options = {
-      url: 'https://localhost:3000/revisionRecord',
+      url: 'http://localhost:3000/revisionRecord',
       method: 'POST',
       headers: 'Content-Type: application/json',
       body: input,
@@ -28,6 +29,33 @@ describe("BackendSDK CareKit Function", () => {
         if(err) {
           console.log(`Error: ${err}`);
         };
+      });
+      return response;
+    }
+    output(options);
+  }),
+  test("Testing GET call to Backend SDK", () => {
+    // code to test goes here
+    const input = require(path.resolve(__dirname + '../../verification.json'));
+    const cacert = fs.readFileSync(path.resolve(__dirname + '../../src/carekit-root.crt'));
+    const options = {
+      url: 'http://localhost:3000/revisionRecord',
+      method: 'GET',
+      headers: 'Content-Type: application/json',
+      body: input,
+      json: true,
+      ca: cacert
+    };
+
+    const output = async (options) => {
+      const response = await request.get(options, function(err, res, body) {
+        if(err) {
+          console.log(`Error: ${err}`);
+        };
+        // if returned json (i.e values stored in the DB) aren't the same as input JSON
+        if (!diff.diff(res, input)){
+          return false;
+        }
       });
       return response;
     }
